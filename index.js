da833c7db8e7135dbb808db74379c79eefe45018
Ankur <ankuryadav7559@gmail.com>
2024-08-31 00:42:14 +0530

    Update index.js
    fix bugs

--- index.js
+++ index.js
@@ -2,90 +2,56 @@ let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
 
-//functionto fetch data from API 
-
-let getMovie = () =>{
-
+// Function to fetch data from API
+let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}
-    `;
-    //if input field is empty
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;  // Updated to https
 
-    if(movieName.length <= 0)
-    {
-        result.innerHTML = `<h3 class="msg">please enter a movie name </h3>`;
-    }else
-    {
-        fetch(url).then((resp) => resp.json()).then((data) =>{
-            // if movie exist in database
-            if(data.Response == "True"){
-                result.innerHTML = `
-                    <div class="info">
-                        <img src=${data.Poster} class="poster">
-                        <div>
-                            <h2>${data.Title}</h2>
-                            <div class="rating">
-                                <img src="star-icon.svg">
-                                <h4>${data.imdbRating}</h4>
-                            </div>
-                            <div class="details">
-                                <span>${data.Rated}</span>
-                                <span>${data.Year}</span>
-                                <span>${data.Runtime}</span>
-                            </div>
-                            <div class="genre">
-                                <div>${data.Genre.split(",").join
-                                ("</div><div>")}</div>
+    // If input field is empty
+    if (movieName.length <= 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a movie name.</h3>`;
+    } else {
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => {
+                // If movie exists in the database
+                if (data.Response === "True") {
+                    result.innerHTML = `
+                        <div class="info">
+                            <img src=${data.Poster} class="poster">
+                            <div>
+                                <h2>${data.Title}</h2>
+                                <div class="rating">
+                                    <img src="star-icon.svg">
+                                    <h4>${data.imdbRating}</h4>
+                                </div>
+                                <div class="details">
+                                    <span>${data.Rated}</span>
+                                    <span>${data.Year}</span>
+                                    <span>${data.Runtime}</span>
+                                </div>
+                                <div class="genre">
+                                    <div>${data.Genre.split(",").join("</div><div>")}</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <h3>Plot:</h3>
-                    <p>${data.Plot}</p>
-                    <h3>Cast:</h3>
-                    <p>${data.Actors}</p>
-                `;
-            }
-            
-            //if movie doesn't exist in database
-            else{
-                result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
-            }
-        })
-        //if error occurs 
-        .catch(()=>{
-            result.innerHTML = `<h3 class="msg"> Error Occured </h3>`;
-        });
+                        <h3>Plot:</h3>
+                        <p>${data.Plot}</p>
+                        <h3>Cast:</h3>
+                        <p>${data.Actors}</p>
+                    `;
+                } else {
+                    // If movie doesn't exist in the database
+                    result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+                }
+            })
+            // If an error occurs
+            .catch((error) => {
+                console.error("API fetch error: ", error);
+                result.innerHTML = `<h3 class="msg">An error occurred. Please try again later.</h3>`;
+            });
     }
-    
 };
 
-searchBtn.addEventListener("click" , getMovie);
-window.addEventListener("load" , getMovie);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+searchBtn.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
